60ff34330979ecf7400178519ce232a01723d929
pat <patrickfungdesigns@gmail.com>
2021-02-24 22:42:21 +1100

    added comical bubble for word

--- Assets/Scripts/LeanTweenAnimation.cs
+++ Assets/Scripts/LeanTweenAnimation.cs
@@ -22,7 +22,7 @@ public class LeanTweenAnimation : MonoBehaviour
     {
         //LeanTween.scale(gameObject, new Vector3(0, 0, 0), 2f).setEase(LeanTweenType.easeOutElastic); 
         
-        LeanTween.scale(gameObject, new Vector3(3, 5, 3), 2f).setEasePunch();        
+        LeanTween.scale(gameObject, new Vector3(1, 1, 1), 2f).setEasePunch();        
         // LeanTween.scale(gameObject, new Vector3(0, 0, 0), 2f).setOnComplete(DestroyMe);
         //LeanTween.alphaText(rhymeWord.GetComponent<RectTransform>(), 10f, 10f);
         // LeanTween.value(rhymeWord)
